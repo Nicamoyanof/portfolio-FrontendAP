01e56f64df9fd249c3295e8535767ea3de93a795
Nicamoyanof <nicamoyanofunes98@gmail.com>
2022-07-24 13:41:14 -0300

    correccion login

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -18,15 +18,18 @@ export class AuthGuard implements CanActivate {
   constructor(private loginService: LoginService, private router: Router) {}
 
   canActivate(): Promise<boolean> {
-    let token = jwt_decode(localStorage.getItem('auth_token'));
-
+    
     return new Promise<boolean>((resolve) => {
       try {
+        let token = jwt_decode(localStorage.getItem('auth_token'));
         let JSONToken = JSON.parse(JSON.stringify(token));
         this.loginService.usuarioAcceso().subscribe((resp) => {
+          console.log('entranding')
           if (resp == 'OK') {
+            console.log('ok')
             resolve(true);
           } else {
+            console.log('error')
             localStorage.removeItem('auth_token');
             this.router.navigate(['login']);
             resolve(false);
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { text } from '@fortawesome/fontawesome-svg-core';
 import jwtDecode from 'jwt-decode';
 import { BehaviorSubject, map, Observable } from 'rxjs';
@@ -8,42 +8,49 @@ import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../models/usuario';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-
   personaLogeada = new EventEmitter();
 
-  API_URL = environment.urlBackend
+  API_URL = environment.urlBackend;
 
   currentUserUserSubject: BehaviorSubject<any>;
-  
-  customHeaders = new HttpHeaders({ Authorization: "Bearer " + localStorage.getItem("auth_token"), 'Access-Control-Allow-Origin': '*'});
 
-  constructor(private http:HttpClient,
-    private router:Router) { 
-      
+  customHeaders = new HttpHeaders({
+    Authorization: 'Bearer ' + localStorage.getItem('auth_token'),
+    'Access-Control-Allow-Origin': '*',
+  });
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(usuario: Usuario) {
+    this.http
+      .post(this.API_URL + `/login`, usuario, { responseType: 'json' })
+      .subscribe((resp: any) => {
+        localStorage.setItem('auth_token', resp.token);
+        console.log(localStorage.getItem('auth_token'));
+        this.usuarioAcceso().subscribe((resp) => {
+          if (resp == 'OK') {
+            this.router.navigate(['admin']);
+          }
+        });
+      });
   }
 
-  login(usuario:Usuario){
-    this.http.post(this.API_URL + `/login`, usuario, {  responseType: 'json' } )
-          .subscribe((resp:any)=>{
-            localStorage.setItem('auth_token', resp.token);
-            this.router.navigate(['admin'])
-          })
-          
-  }
-
-  getPersonaLogged(usuario:string){
-    if(usuario!=null){
-      return this.http.get(this.API_URL + `/usuarioLog/${usuario}`, {headers : this.customHeaders})
+  getPersonaLogged(usuario: string) {
+    if (usuario != null) {
+      return this.http.get(this.API_URL + `/usuarioLog/${usuario}`, {
+        headers: this.customHeaders,
+      });
     }
     return null;
   }
 
-  usuarioAcceso(){
-    return this.http.get(this.API_URL + `/access`, {headers : this.customHeaders})
+  usuarioAcceso() {
+    console.log(localStorage.getItem('auth_token'), 'segunda');
+    return this.http.get(this.API_URL + `/access`, {
+      headers: this.customHeaders,
+    });
   }
-
-
 }
